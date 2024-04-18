22ded6210cf28b2980d8732801f939e76e585745
jeniferleleany.meyer <jeniferleleany.meyer@uzh.ch>
2024-04-18 13:13:06 +0200

    models n samples

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,22 +1,16 @@
 #! /bin/bash
-
 scripts=$(dirname "$0")
-base=$(realpath $scripts/..)
-
+base=$(realpath $scripts)
 models=$base/models
 data=$base/data
 tools=$base/tools
 samples=$base/samples
-
-mkdir -p $samples
-
+# mkdir scripts/samples
 num_threads=4
 device=""
-
-(cd $tools/pytorch-examples/word_language_model &&
-    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python generate.py \
-        --data $data/grimm \
-        --words 100 \
-        --checkpoint $models/model.pt \
-        --outf $samples/sample
-)
+CUDA_VISIBLE_DEVICES="" OMP_NUM_THREADS=4 python3 scripts/tools/pytorch-examples/word_language_model/generate.py --data scripts/data/shakespeare --words 100 --checkpoint scripts/models/model.pt --outf scripts/samples/sample.txt
+CUDA_VISIBLE_DEVICES="" OMP_NUM_THREADS=4 python3 scripts/tools/pytorch-examples/word_language_model/generate.py --data scripts/data/shakespeare --words 100 --checkpoint scripts/models/model00.pt --outf scripts/samples/sample00.txt
+CUDA_VISIBLE_DEVICES="" OMP_NUM_THREADS=4 python3 scripts/tools/pytorch-examples/word_language_model/generate.py --data scripts/data/shakespeare --words 100 --checkpoint scripts/models/model01.pt --outf scripts/samples/sample01.txt
+CUDA_VISIBLE_DEVICES="" OMP_NUM_THREADS=4 python3 scripts/tools/pytorch-examples/word_language_model/generate.py --data scripts/data/shakespeare --words 100 --checkpoint scripts/models/model03.pt --outf scripts/samples/sample03.txt
+CUDA_VISIBLE_DEVICES="" OMP_NUM_THREADS=4 python3 scripts/tools/pytorch-examples/word_language_model/generate.py --data scripts/data/shakespeare --words 100 --checkpoint scripts/models/model06.pt --outf scripts/samples/sample06.txt
+CUDA_VISIBLE_DEVICES="" OMP_NUM_THREADS=4 python3 scripts/tools/pytorch-examples/word_language_model/generate.py --data scripts/data/shakespeare --words 100 --checkpoint scripts/models/model09.pt --outf scripts/samples/sample09.txt
